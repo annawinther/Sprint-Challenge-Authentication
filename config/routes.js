89826893ab925ed219a9f398ce37ad24bd581127
Anna W <anski.anna@gmail.com>
2019-07-26 09:24:46 +0200

    register new user works

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
-
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -10,11 +11,25 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 10);
+  credentials.password = hash;
+
+  db('users')
+    .insert(credentials)
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ newUserId: id})
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'could not register user' });
+    })
 }
 
 function login(req, res) {
   // implement user login
 }
+
 
 function getJokes(req, res) {
   const requestOptions = {
