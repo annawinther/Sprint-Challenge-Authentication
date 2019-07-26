45ff811d2fc4e53f350028557ebef7ed88929efe
Anna W <anski.anna@gmail.com>
2019-07-26 09:40:55 +0200

    login working

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
 const db = require('../database/dbConfig');
+const jwt = require('jsonwebtoken');
+const secret = require('./secret');
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -9,17 +11,24 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
+ function register(req, res) {
   // implement user registration
   const credentials = req.body;
   const hash = bcrypt.hashSync(credentials.password, 10);
   credentials.password = hash;
 
+//   try {
+//     const users = await db.insert(credentials);
+//       res.status(201).json(users)
+//   } catch (error) {
+//       res.status(500).json({ message: 'could not register new user'})
+//     }
+// }
   db('users')
     .insert(credentials)
     .then(ids => {
       const id = ids[0];
-      res.status(201).json({ newUserId: id})
+      res.status(201).json({newUserId: id, credentials})
     })
     .catch(err => {
       res.status(500).json({ message: 'could not register user' });
@@ -28,7 +37,37 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  let { username, password} = req.body;
+ db('users')
+  .where({ username })
+  .first()
+  .then(user => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
+          res.status(200).json({ 
+              message: `You're now logged in as ${user.username}`,
+              token: token
+          })
+      } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+      }
+  })
+  .catch(error => {
+      res.status(500).json({ message: 'could not log in this user' });
+  })
 }
+
+function generateToken(user){
+  const payload = {
+      sub: user.id,
+      username: user.username
+  }
+  const options = {
+      expiresIn: '1d'
+  }
+  return jwt.sign(payload, secret.jwtSecrets, options)
+}
+
 
 
 function getJokes(req, res) {
@@ -45,3 +84,4 @@ function getJokes(req, res) {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
 }
+
